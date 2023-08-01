74e851a2f3921b0e3245630b8e2c3a3028776556
tanaliga100 <jordantanaliga@gmail.com>
2023-08-01 15:21:33 +0800

    for-prod

--- client/src/components/shared/Hero.tsx
+++ client/src/components/shared/Hero.tsx
@@ -2,11 +2,13 @@ import { Heading } from "@chakra-ui/react";
 import React from "react";
 
 const Hero: React.FC<IProps> = (props) => {
+  console.log(props);
+
   //   const theme = useTheme();
-  const path = props?.text.slice(1, props.text?.length);
+  //   const path = props?.text.slice(1, props.text?.length);
   return (
     <Heading as="p" size="sm">
-      {path}
+      {/* {path} */}
     </Heading>
   );
 };
--- client/src/components/wrapper/LoadingWrapper.tsx
+++ client/src/components/wrapper/LoadingWrapper.tsx
@@ -1,21 +1,4 @@
-import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
-import React from "react";
-
-interface IProps {
-  isLoading?: boolean;
-  text?: string;
-}
-
-export const LoadingWrapper: React.FC<IProps> = (props) => {
-  console.log(props);
-
-  return (
-    <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={false}
-    >
-      <CircularProgress color="inherit" />
-    </Backdrop>
-  );
+const LoadingWrapper = () => {
+  return <div>LoadingWrapper</div>;
 };
+export default LoadingWrapper;
